b0e5abc01e813258b81fd887cbaa76447bd8c046
ernestynne <ernestynne@nicholsonconsulting.co.nz>
2017-11-22 21:02:08 +1300

    issue fix #4 rprogs template

---
+++ rprogs/rprogs_template.R
@@ -0,0 +1,21 @@
+# ======================================================================================================= #
+# DESCRIPTION: <description of what the program does>
+# 
+# INPUT:
+# <var_name> = <var_description>
+# 
+# OUTPUT:
+# <var_name> = <var_description>
+#
+# AUTHOR: <author_name in first initial surname format>
+# 
+# DEPENDENCIES:
+# <TBA>
+# 
+# NOTES: 
+# <TBA>
+# 
+# HISTORY (reverse order):
+#
+# <dd BBB YYYY XX initial version>
+# ======================================================================================================= #
